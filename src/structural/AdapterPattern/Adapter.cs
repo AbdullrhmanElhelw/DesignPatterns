3dc5588b404a20345a0c14100c1c4c010c70ddda
AbdullrhmanElhelw <elhelw258@gmail.com>
2024-12-11 00:09:47 +0200

    implement adapter pattern

---
+++ src/structural/AdapterPattern/Adapter.cs
@@ -0,0 +1,16 @@
+﻿namespace AdapterPattern;
+
+public class Adapter : PaymentProcessorTarget
+{
+    private readonly ThirdPartyPayment _thirdPartyPayment;
+
+    public Adapter(ThirdPartyPayment thirdPartyPayment)
+    {
+        _thirdPartyPayment = thirdPartyPayment;
+    }
+
+    public override void Pay(decimal amount)
+    {
+        _thirdPartyPayment.Pay(amount);
+    }
+}
